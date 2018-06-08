0387fd9ac647323031ea2d38ff0332a89c88e91e
Matt <makeshifthidalgo@hotmail.com>
2018-06-08 13:30:25 -0700

    styling done

---
+++ assets/javascript/logic.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBdSYeMjm27VEXz_JjcFGlzQDUTxCQ5nrQ",
+    authDomain: "trains-e8129.firebaseapp.com",
+    databaseURL: "https://trains-e8129.firebaseio.com",
+    projectId: "trains-e8129",
+    storageBucket: "",
+    messagingSenderId: "1094974192529"
+  };
+  firebase.initializeApp(config);
+
+
+  var database = firebase.database();
+
+  
+
+
+
+
+
+
+
+})
